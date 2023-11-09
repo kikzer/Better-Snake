7fc08ef75885d3c82a9780f3b87d0a3dd6d9d4ae
tl069 <tl069@hdm-stuttgart.de>
2023-11-09 14:07:41 +0100

    Changed Main Class to launch the GameWindow class, so that only the main Method in Main is needed.

--- src/main/java/mainpackage/Main.java
+++ src/main/java/mainpackage/Main.java
@@ -1,26 +1,10 @@
 package mainpackage;
 
+import Managment.Interface.GameWindow;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.Objects;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        URL fxmlFileUrl = getClass().getClassLoader().getResource("sample.fxml");
-        Parent root = FXMLLoader.load(Objects.requireNonNull(fxmlFileUrl));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(GameWindow.class, args);
     }
 }
