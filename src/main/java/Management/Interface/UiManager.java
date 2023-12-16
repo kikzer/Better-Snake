7d4e49466cad715d40397d527c846e468a2efd73
Kieß Niklas <nk150@hdm-stuttgart.de>
2023-12-16 21:03:24 +0100

    Niklas branch

--- src/main/java/Management/Interface/UiManager.java
+++ src/main/java/Management/Interface/UiManager.java
@@ -12,16 +12,16 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class UiManager {
-    private ArrayList<Scene> scenes = new ArrayList<>();
+    private final ArrayList<Scene> scenes = new ArrayList<>();
     private Stage currentStage;
-    private Label scoreField = new Label();
+    private final Label scoreField = new Label();
     private static UiManager instance;
 
-    private UiManager(){
+    private UiManager() {
         scoreField.setTextFill(Color.BLACK);
         scoreField.setFont(new Font("Balloon", 24));
         scoreField.setTranslateY(GameWindow.HEIGHT);
-        scoreField.setTranslateX(GameField.SIZEBLOCK*2);
+        scoreField.setTranslateX(GameField.SIZEBLOCK * 2);
     }
 
     public static UiManager getInstance() {
@@ -35,31 +35,14 @@ public class UiManager {
         GameWindow.getInstance().updateBackground();
         Snake.getInstance().draw(GameWindow.getInstance().getGraphicContext());
         ObjectManager.getInstance().getCurrentFood().show(GameWindow.getInstance().getGraphicContext());
-        ObjectManager.getInstance().getCurrenObject().show(GameWindow.getInstance().getGraphicContext());
+        if (ObjectManager.getInstance().getCurrenObject() != null)
+            ObjectManager.getInstance().getCurrenObject().show(GameWindow.getInstance().getGraphicContext());
     }
 
-    public Stage getCurrentStage() {
-        return currentStage;
-    }
 
-    public ArrayList<Scene> getScenes() {
-        return scenes;
-    }
-
-    public void setScenes(ArrayList<Scene> scenes) {
-        this.scenes = scenes;
-    }
-
-    public void setCurrentStage(Stage currentStage) {
-        this.currentStage = currentStage;
-    }
 
     public Label getScoreField() {
         return scoreField;
-    }
-
-    public void setScoreField(Label scoreField) {
-        this.scoreField = scoreField;
     }
 
 }
