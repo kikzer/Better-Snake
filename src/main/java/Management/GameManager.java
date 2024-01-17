cd83dde8df874f89a0be1c974617b6447826f1f5
kikzer <niklas.kiess@web.de>
2024-01-17 17:30:33 +0100

    moved all JavaFx related Methods to UiManager

--- src/main/java/Management/GameManager.java
+++ src/main/java/Management/GameManager.java
@@ -136,23 +136,7 @@ public class GameManager {
         checkWinningCondition();
         if (Snake.getInstance().isGameOver() && !switchScene && Snake.getInstance().getPositions().size() == 1) {
             switchScene = true;
-            //gameOver();
-
         }
-    }
-
-    private void gameOver() {
-        Platform.runLater(() -> {
-            try {
-                Stage currentStage = (Stage) GameWindow.getInstance().getGameScene().getWindow();
-                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GameOverScene.fxml")));
-                Scene gameOverScene = new Scene(root);
-                currentStage.setScene(gameOverScene);
-                currentStage.show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
     }
 
     public Timer getGameTick() {
@@ -238,15 +222,6 @@ public class GameManager {
 
         } else {
             Snake.getInstance().setGrowing(false);
-        }
-        if(Platform.isFxApplicationThread()){
-            Platform.runLater(() -> {
-                if (Score.getInstance().getHighScore() <= Score.getInstance().getScore()) {
-                    Score.getInstance().setHighScore(Score.getInstance().getScore());
-                }
-                UiManager.getInstance().getScoreField().setText("Score: " + Score.getInstance().getScore());
-                UiManager.getInstance().getHighScoreField().setText("HighScore: " + Score.getInstance().getHighScore());
-            });
         }
     }
 
